c3c77ccce1ea6f048be5d6b864dec775ff7c2dde
taiwo-eng <taiwoakinnusoye@outlook.com>
2023-04-21 22:06:44 +0100

    adding css styles

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 export default function Nav() {
     return (
         <nav>
-            <Link href='products'>Products</Link>
-            <Link href='sell'>Sell</Link>
-            <Link href='orders'>Orders</Link>
-            <Link href='account'>Account</Link>
+            <Link href='/products'>Products</Link>
+            <Link href='/sell'>Sell</Link>
+            <Link href='/orders'>Orders</Link>
+            <Link href='/account'>Account</Link>
         </nav>
     )
 }
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,13 @@
 import Document, {Html, Main, Head, NextScript} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class ArtStoreDocument extends Document {
+    static getInitialProps( { renderPage }) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props =>  sheet.collectStyles(<App {...props} />));
+        const styleTags =sheet.getStyleElement();
+        return {...page, styleTags}
+    }
     render() {
         return (
         <Html lang="en-GB">
