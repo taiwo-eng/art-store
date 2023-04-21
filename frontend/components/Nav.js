9a3ca2422921d3c05dfa1c368beceea4ef661ba9
taiwo-eng <taiwoakinnusoye@outlook.com>
2023-04-21 21:25:19 +0100

    adding css styles

---
+++ frontend/components/Nav.js
@@ -0,0 +1,10 @@
+export default function Nav() {
+    return (
+        <nav>
+            <Link href='products'>Products</Link>
+            <Link href='sell'>Sell</Link>
+            <Link href='orders'>Orders</Link>
+            <Link href='account'>Account</Link>
+        </nav>
+    )
+}
---
+++ frontend/pages/_document.js
@@ -0,0 +1,15 @@
+import Document, {Html, Main, Head, NextScript} from 'next/document';
+
+export default class ArtStoreDocument extends Document {
+    render() {
+        return (
+        <Html lang="en-GB">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+        )
+    }
+}
