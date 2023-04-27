51300048be5b89caf7ea037aef07c044ca29a89b
taiwo-eng <taiwoakinnusoye@outlook.com>
2023-04-27 22:55:21 +0100

    add delete from apollo cache function

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -10,8 +10,12 @@ const DELETE_PRODUCT_MUTATION = gql`
     }
 `;
 
+function update(cache, payload) {
+    cache.evict(cache.identify(payload.data.deleteProduct))
+}
+
 export default function DeleteProduct ({ id, children }) {
-    const [deleteProduct, {loading}] = useMutation(DELETE_PRODUCT_MUTATION, {variables: {id}})
+    const [deleteProduct, {loading}] = useMutation(DELETE_PRODUCT_MUTATION, {variables: {id}, update: update()})
     return (
         <button disabled={loading} type="button" onClick={function () {
             if (confirm('Are you sure you want to delete this item?')) {
