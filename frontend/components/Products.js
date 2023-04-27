e47d9ad6af98e1ffe058517a178f759bc77ac434
taiwo-eng <taiwoakinnusoye@outlook.com>
2023-04-28 00:03:47 +0100

    add pqge number query for pagination

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -2,10 +2,11 @@ import gql from 'graphql-tag';
 import {useQuery} from '@apollo/client';
 import { ProductListStyles } from './styles/ProductListStyles';
 import Product from './Product';
+import { perPage } from '../config';
 
 export const ALL_PRODUCTS_QUERY = gql`
-    query ALL_PRODUCTS_QUERY {
-    allProducts {
+    query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
         id
         name
         description
@@ -20,8 +21,13 @@ export const ALL_PRODUCTS_QUERY = gql`
 }
 `;
 
-export default function Products () {
-    const {data, error, loading} = useQuery(ALL_PRODUCTS_QUERY);
+export default function Products ({page}) {
+    const {data, error, loading} = useQuery(ALL_PRODUCTS_QUERY, {
+        variables: {
+            first: perPage,
+            skip: page * perPage - perPage
+        }
+    });
     if (loading) return <p>Loading.....</p>
     if (error) return <p>{error}</p>
     return (
