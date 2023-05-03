b6ea1f27b586db8817b890e973c441af2314f4bf
taiwo-eng <taiwoakinnusoye@outlook.com>
2023-05-03 07:25:52 +0100

    sign in component and query

--- frontend/components/SignIn.js
+++ frontend/components/SignIn.js
@@ -1,38 +1,50 @@
 import Form from './styles/Form';
 import useForm from "../lib/useForm";
-import { useMutation } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
 import { CURRENT_USER_QUERY } from './User';
+import Error from "./ErrorMessage";
 
 const SIGNIN_MUTATION = gql`
     mutation SIGNIN_MUTATION ($email: String!, $password: String!){
-        ... on authenticationWithPasswordSuccess {
+        authenticateUserWithPassword (email: $email, password: $password) {
+        ... on UserAuthenticationWithPasswordSuccess {
             item {
                 id
                 email
                 name
             }
         }
+        ... on UserAuthenticationWithPasswordFailure {
+            code
+            message
+        }
+        }
     }
 `;
 
 export default function SignIn () {
-    const {inputs, handleChange} = useForm({
+    const {inputs, handleChange, resetForm} = useForm({
         email: "",
         password: ""
     });
 
-    const [sigin, {data, error, loading}] = useMutation(SIGNIN_MUTATION, {
+    const [signin, {data, loading}] = useMutation(SIGNIN_MUTATION, {
         variables: inputs,
         refetchQueries: [{ query: CURRENT_USER_QUERY}]
     })
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
+        await signin();
+        resetForm();
     }
+
+    const error = data?.authenticateUserWithPassword.__typename === "UserAuthenticationWithPasswordFailure" ? data?.authenticateUserWithPassword : undefined;
 
     return (
         <Form method='POST' onSubmit={handleSubmit}>
             <h2>Sign In To Your Account</h2>
+            <Error error={error} />
             <fieldset>
                 <label htmlFor='email'>
                     Email
