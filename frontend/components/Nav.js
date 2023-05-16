794ff27ead3193f2499a09c6744849d35d079fe2
taiwo-eng <taiwoakinnusoye@outlook.com>
2023-05-16 19:18:31 +0100

    add sign out mutation

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
+import SignOut from './SignOut';
 
 export default function Nav() {
     const user = useUser()
@@ -11,7 +12,9 @@ export default function Nav() {
             <>
                 <Link href='/sell'>Sell</Link>
                 <Link href='/orders'>Orders</Link>
-                <Link href='/account'>Account</Link></>
+                <Link href='/account'>Account</Link>
+                <SignOut />
+            </>
            )}
            {!user && (
             <>
